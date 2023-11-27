4bac3c457384e4153e82ada079c79008240d0e76
selvinkumaran <selvin.sellappan@kumaran.com>
2023-11-27 10:12:31 +0530

    Payment completed

--- src/app/component/user/payment/payment.component.ts
+++ src/app/component/user/payment/payment.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AppResponse } from 'src/app/model/appResponse';
 import { Payment } from 'src/app/model/payment';
 import { PaymentService } from 'src/app/service/payment.service';
 import { StorageService } from 'src/app/service/storage.service';
-
 import lottie from 'lottie-web';
-import { ActivatedRoute, Router } from '@angular/router';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { ActivatedRoute } from '@angular/router';
+import { LoaderService } from 'src/app/service/loader.service';
 
 @Component({
   selector: 'app-payment',
@@ -14,11 +16,25 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent {
+  showLottie: boolean = false;
+
+  @ViewChild(LottieComponent) lottieComponent!: LottieComponent;
+
   constructor(
     private paymentService: PaymentService,
     private storageService: StorageService,
-    private route: ActivatedRoute,private router:Router
+    private route: ActivatedRoute,
+    private router: Router,
+    loaderService: LoaderService
   ) {}
+
+  options: AnimationOptions = {
+    path: '/assets/success.json',
+    rendererSettings: {
+      className: 'lottie-loader',
+    },
+  };
+
   paymentDetails: Payment[] = [];
   error: string = '';
   param: number | null = null;
@@ -37,11 +53,10 @@ export class PaymentComponent {
 
   btnText: string = 'Book Now';
 
-  onSubmit(todoForm: NgForm) {}
+  onSubmit(paymentForm: NgForm) {}
 
   loggedInUser = this.storageService.getLoggedInUser();
-
-  book(paymentForm: NgForm) {
+  book(_paymentForm: NgForm) {
     this.route.queryParams.subscribe((params) => {
       this.param = params['totalPayment'];
       let payment: Payment = {
@@ -55,8 +70,7 @@ export class PaymentComponent {
         userId: this.loggedInUser.id,
         amount: this.param!,
       };
-      console.log(payment.amount,"new amount");
-      
+
       this.paymentService.postpayment(payment).subscribe({
         next: (_response: AppResponse) => {},
         error: (err) => {
@@ -64,11 +78,17 @@ export class PaymentComponent {
           let message: string = err.error.error.message;
           this.error = message.includes(',') ? message.split(',')[0] : message;
         },
-        complete: () => console.log('There are no more action happen.'),
+        complete: () => {
+          console.log('There are no more action happen.');
+        },
       });
     });
   }
+
   navigateToBooking() {
-    this.router.navigate(['/booking']);
+    this.showLottie = true;
+    setTimeout(() => {
+      this.router.navigate(['/booking']);
+    }, 3000);
   }
 }
